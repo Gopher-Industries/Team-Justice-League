8290e2f037a94a04fc1a7e1cd08c787c3ac85a41
Nadav Fedida <62866537+nadavfedida@users.noreply.github.com>
2023-04-27 21:44:51 +1000

    Changing webcam module

--- RnD/Face Quality/Webcam.py
+++ RnD/Face Quality/Webcam.py
@@ -9,19 +9,25 @@ def get_image():
     cam = cv2.VideoCapture(1)
     cv2.namedWindow("Pain detection application")
     img_counter = 0
-
+    
+    if not cam.isOpened():
+        raise IOError("Cannot open webcam")
+    
     while img_counter < 1:
         ret, frame = cam.read()
         if not ret:
-            print("failed to grab frame")
-            break
+            raise Exception("failed to grab frame")
+        
         cv2.imshow("Pain detection application", frame)
 
         k = cv2.waitKey(1)
-        # if k%256 == 27:
-        #     # ESC pressed
-        #     print("Escape hit, closing...") 
-        #     break
+        if k%256 == 27:
+            # ESC pressed
+            print("Escape hit, closing...") 
+            cam.release()
+            cv2.destroyAllWindows()
+            raise Exception("Window terminated")
+            return 
         if k%256 == 32:
             # SPACE pressed
             dateTimenow = f"{datetime.now() :%Y-%m-%d_%H-%M-%S}"
@@ -31,9 +37,10 @@ def get_image():
             print("{} written!".format(img_name))
             img_counter += 1
     
-    cam.release()
-    cv2.destroyAllWindows()
-    return img_name, frame
+            cam.release()
+            cv2.destroyAllWindows()
+            return img_name, frame
+    raise Exception("no image available")
 
 
 
